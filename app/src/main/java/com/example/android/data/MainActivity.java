11132ed776026685be84f6f897aa24c3946967ea
John <johnm.staana@gmail.com>
2017-08-29 16:21:46 +1200

    Images and recyclerview Updated

--- app/src/main/java/com/example/android/data/MainActivity.java
+++ app/src/main/java/com/example/android/data/MainActivity.java
@@ -1,27 +1,24 @@
 package com.example.android.data;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import com.example.android.data.database.DBHelper;
 import com.example.android.data.database.DataSource;
 import com.example.android.data.model.DataItem;
 import com.example.android.data.sample.SampleDataProvider;
 
-import java.util.Collections;
-import java.util.Comparator;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    private static final int SIGNIN_REQUEST = 1001;
+    public static final String MY_GLOBAL_PREFS = "my_global_prefs";
+    private static final String TAG = "MainActivity";
     List<DataItem> dataItemList = SampleDataProvider.dataItemList;
+
 
     DataSource mDatasource;
 
